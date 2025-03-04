3d566322195fb0a15517201b423cc279ecbc9c60
Gunanka D <gunanka2594@gmail.com>
2025-03-04 19:19:05 +0530

    render URL updation
    + requirements.txt updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-import subprocess
+import subprocess, os
 
 
 app = Flask(__name__)
@@ -36,4 +36,5 @@ def decrypt():
 
 
 if (__name__ == '__main__'):
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get Render's assigned port
+    app.run(host="0.0.0.0", port=port, debug=False)  # Bind to 0.0.0.0
